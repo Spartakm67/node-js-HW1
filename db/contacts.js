aadc2e82ef865c0c3223dad3afcfb08bdbb80e8d
Spartak Maznytskyi <109909363+Spartakm67@users.noreply.github.com>
2023-05-03 00:16:43 +0300

    Path Added

--- db/contacts.js
+++ db/contacts.js
@@ -1,2 +1,4 @@
-const fs = require("fs/promises");
-const path = require("path");
+// const fs = require("fs/promises");
+const fs = require('fs').promises;
+// const path = require("path");
+const path = require('node:path');
